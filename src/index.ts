7eae77657c4e81a16c67f4e88c3fa9a0ded36c9e
on21viktoria <viktoria.seubert@gmx.de>
2022-04-29 10:11:03 +0200

    added logic that checks if selected cards match

--- src/index.ts
+++ src/index.ts
@@ -3,92 +3,139 @@ import { helloWorld, Beispiel } from "./myModule";
 import { MemoryCard } from "./MemoryCard.interface";
 
 const cards: MemoryCard[] = [
-
   {
-      cardId: 'breaking-bad',
-      image: './src/assets/cards/breaking_bad.jpg',
-      count: 0,
-      selected: false,
-      set: false
+    cardId: "breaking-bad",
+    image: "./src/assets/cards/breaking_bad.jpg",
+    count: 0,
+    selected: false,
+    set: false,
   },
   {
-      cardId: "damengambit",
-      image: "./src/assets/cards/damengambit.jpg",
-      count: 0,
-      selected: false,
-      set: false
+    cardId: "damengambit",
+    image: "./src/assets/cards/damengambit.jpg",
+    count: 0,
+    selected: false,
+    set: false,
   },
   {
-      cardId: "friends",
-      image: "./src/assets/cards/friends.jpg",
-      count: 0,
-      selected: false,
-      set: false
+    cardId: "friends",
+    image: "./src/assets/cards/friends.jpg",
+    count: 0,
+    selected: false,
+    set: false,
   },
   {
-      cardId: "game-of-thrones",
-      image: "./src/assets/cards/game_of_thrones.jpg",
-      count: 0,
-      selected: false,
-      set: false
+    cardId: "game-of-thrones",
+    image: "./src/assets/cards/game_of_thrones.jpg",
+    count: 0,
+    selected: false,
+    set: false,
   },
   {
-      cardId: "haus-des-geldes",
-      image: "./src/assets/cards/haus_des_geldes.jpg",
-      count: 0,
-      selected: false,
-      set: false
+    cardId: "haus-des-geldes",
+    image: "./src/assets/cards/haus_des_geldes.jpg",
+    count: 0,
+    selected: false,
+    set: false,
   },
   {
-      cardId: "rick_and_morty",
-      image: "./src/assets/cards/rick_and_morty.JPG",
-      count: 0,
-      selected: false,
-      set: false
+    cardId: "rick_and_morty",
+    image: "./src/assets/cards/rick_and_morty.JPG",
+    count: 0,
+    selected: false,
+    set: false,
   },
   {
-      cardId: "star-trek",
-      image: "./src/assets/cards/star_trek.jpg",
-      count: 0,
-      selected: false,
-      set: false
+    cardId: "star-trek",
+    image: "./src/assets/cards/star_trek.jpg",
+    count: 0,
+    selected: false,
+    set: false,
   },
   {
-      cardId: "the-office",
-      image: "./src/assets/cards/the_office.jpg",
-      count: 0,
-      selected: false,
-      set: false
-  }
+    cardId: "the-office",
+    image: "./src/assets/cards/the_office.jpg",
+    count: 0,
+    selected: false,
+    set: false,
+  },
 ];
 
-const cardsContainers = document.querySelectorAll<HTMLImageElement>(".memory-card");
+const cardsContainers =
+  document.querySelectorAll<HTMLImageElement>(".memory-card");
+let selectedCards: HTMLImageElement[] = [];
+let score: number = 0;
 
 assignRandomSpotsForCards();
 
-function assignRandomSpotsForCards() : void {
+function assignRandomSpotsForCards(): void {
   let memoryCardsSpots: MemoryCard[] = [];
 
-  while(memoryCardsSpots.length < 16){
-    let generatedCard = cards[Math.floor(Math.random() * cards.length)]
+  while (memoryCardsSpots.length < 16) {
+    let generatedCard = cards[Math.floor(Math.random() * cards.length)];
 
-    if(generatedCard.count < 2){
-      memoryCardsSpots.push(generatedCard) 
+    if (generatedCard.count < 2) {
+      memoryCardsSpots.push(generatedCard);
       generatedCard.count++;
-    }  
+    }
   }
-  for(let i = 0; i < cardsContainers.length; i++){
-    cardsContainers[i].setAttribute("src", memoryCardsSpots[i].image)
-    cardsContainers[i].setAttribute("alt", memoryCardsSpots[i].cardId)
+  for (let i = 0; i < cardsContainers.length; i++) {
+    cardsContainers[i].setAttribute("src", memoryCardsSpots[i].image);
+    cardsContainers[i].setAttribute("alt", memoryCardsSpots[i].cardId);
+    cardsContainers[i].setAttribute("data-id", memoryCardsSpots[i].cardId);
   }
 }
 
-cardsContainers.forEach(cardsContainer => {
+cardsContainers.forEach((cardsContainer) => {
   cardsContainer.addEventListener("click", selectCard);
 });
 
 function selectCard(e: UIEvent) {
   const currentCard = e.target as HTMLImageElement;
-  console.log(e.target)
-  currentCard.parentElement?.classList.add("selected");
+  currentCard.removeEventListener("click", selectCard);
+
+  selectedCards.push(currentCard);
+
+  selectedCards.forEach((selectedCard) => {
+    console.log(selectedCard);
+    selectedCard.parentElement?.classList.add("selected")
+  });
+
+  if (selectedCards.length === 2) {
+    checkforMatch();
+  }
+}
+
+function checkforMatch() {
+  if (
+    selectedCards[0].getAttribute("data-id") ===
+    selectedCards[1].getAttribute("data-id")
+  ) {
+    console.log("Match");
+    selectedCards.forEach((selectedCard) => {
+    selectedCard.parentElement?.classList.remove("selected");
+    selectedCard.parentElement?.classList.add("set");
+    selectedCard.removeEventListener("click", selectCard);
+    });
+
+    setScore();
+  } else {
+    console.log("no match");
+    selectedCards.forEach((selectedCard) => {
+      selectedCard.parentElement?.classList.remove("selected");
+      selectedCard.addEventListener("click", selectCard);
+    });
+  }
+
+  selectedCards = []
+}
+
+function setScore() {
+  score++;
+  let scoreElement = document.querySelector(".score") as HTMLHeadingElement
+  scoreElement.innerHTML = score.toString();
+
+  if(score === 8) {
+    console.log("You won!")
+  }
 }
